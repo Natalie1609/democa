132289592f80a2a6a715415393e68835465d346e
Natalie Alexandrova <bns1609@gmail.com>
2022-11-22 21:25:44 +0300

    added file update PracticeFormTests

--- src/test/java/PracticeFormTests.java
+++ src/test/java/PracticeFormTests.java
@@ -1,5 +1,48 @@
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
 public class PracticeFormTests {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.browserSize = "7680x4320";
+        Configuration.baseUrl = "https://demoqa.com";
+    }
+    @Test
     void fillFormTest() {
-        open("https://demoqa.com/automation-practice-form")
+        open("/automation-practice-form");
+
+        $("#firstName").setValue("Natalie");
+        $("#lastName").setValue("Alexandrova");
+        $("#userEmail").setValue("123@gmail.com");
+        $("#gender-radio-2").doubleClick();
+        $("#userNumber").setValue("89556664433");
+//      Выбор днюхи:
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").click();
+        $(".react-datepicker__month-select").selectOption("September");
+        $(".react-datepicker__year-select").click();
+        $(".react-datepicker__year-select").selectOption("1995");
+        $(".react-datepicker__day--016").click();
+
+        $("#subjectsInput").click();
+        $("#subjectsInput").setValue("H");
+        $("#subjectsInput").pressEnter();
+
+        $("[for=hobbies-checkbox-2]").click();
+        $("#uploadPicture").uploadFile(new File("src/test/java/test.txt");
+        $("#currentAddress").setValue("SPB");
+//        $("#submit").click();
+
+//        $(".modal-dialog modal-lg").shouldBe(Condition.visible);
+//        $().shouldHave(Text(""));
     }
 }
