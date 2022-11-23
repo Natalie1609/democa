2a4bbe0cced8809548f68c43ddb7be0b42c113e8
Natalie Alexandrova <bns1609@gmail.com>
2022-11-23 10:16:32 +0300

    upd PracticeFormTests Autotest finish

--- src/test/java/PracticeFormTests.java
+++ src/test/java/PracticeFormTests.java
@@ -1,10 +1,12 @@
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.selector.ByText;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
@@ -13,7 +15,7 @@ public class PracticeFormTests {
     @BeforeAll
     static void beforeAll() {
         Configuration.holdBrowserOpen = true;
-        Configuration.browserSize = "1000x800";
+        Configuration.browserSize = "700x1200";
         Configuration.baseUrl = "https://demoqa.com";
     }
     @Test
@@ -24,7 +26,7 @@ public class PracticeFormTests {
         $("#lastName").setValue("Alexandrova");
         $("#userEmail").setValue("123@gmail.com");
         $("#gender-radio-2").doubleClick();
-        $("#userNumber").setValue("89556664433");
+        $("#userNumber").setValue("9556664433");
 //      Выбор днюхи:
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").click();
@@ -38,15 +40,27 @@ public class PracticeFormTests {
         $("#subjectsInput").pressEnter();
 
         $("[for=hobbies-checkbox-2]").click();
-      //  $("#uploadPicture").uploadFile(new File("src/test/java/Files/test.png");
+        $("#uploadPicture").uploadFile(new File("src/test/Files/test.png"));
         $("#currentAddress").setValue("SPB");
         $("#state").click();
-        $("#state").setValue("H");
-        $("#state").pressTab();
-
+        $("#stateCity-wrapper").$(new ByText("Uttar Pradesh")).click();
+        $("#city").click();
+        $("#stateCity-wrapper").$(new ByText("Merrut")).click();
         $("#submit").click();
 
-        $(".modal-dialog modal-lg").shouldBe(Condition.visible);
-//        $().shouldHave(Text(""));
+        // Проверки
+        $(".modal-title").shouldHave(text("Thanks for submitting the form"));
+        $(".table-responsive").shouldHave(
+                text("Natalie Alexandrova"),
+                text("123@gmail.com"),
+                text("Female"), text("9556664433"),
+                text("16 September,1995"),
+                text("Reading"),
+                text("test.PNG"),
+                text("SPB"),
+                text("Uttar Pradesh Merrut")
+        );
+        $("#closeLargeModal").click();
     }
 }
+
